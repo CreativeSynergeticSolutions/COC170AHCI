7ba46612835f44302bae8ef0e3a181e8282c5f35
Matthew Elphick <mae.me.uk@gmail.com>
2014-10-14 12:51:22 +0100

    Fix and simplify menu javascript

--- D1/javascripts/main.js
+++ D1/javascripts/main.js
@@ -1,14 +1,12 @@
-var menu = {
+menu = {
   sidebar: document.getElementById('sidebar-toggle'),
   element: document.getElementById('sidebar-toggle').parentElement,
   open: function(){
     this.element.className = this.element.className.replace('menu-closed','menu-open');
-    this.sidebar.className = this.sidebar.className.replace('sidebar-closed','sidebar-open');
     this.sidebar.getElementsByTagName('i')[0].className = 'icon-left-open';
   },
   close: function(){
     this.element.className = this.element.className.replace('menu-open','menu-closed');
-    this.sidebar.className = this.sidebar.className.replace('sidebar-open','sidebar-closed');
     this.sidebar.getElementsByTagName('i')[0].className = 'icon-right-open';
   },
   toggle: function(){
