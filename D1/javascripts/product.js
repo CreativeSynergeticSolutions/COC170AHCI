fa4a85eb62f3204eb8ddbcfd2ac98acba8c1da68
Johnbastian <j.emilianus-12@student.lboro.ac.uk>
2014-10-17 01:32:52 +0100

    Save, Add to Basket, and Delivery Msgs added

--- D1/javascripts/product.js
+++ D1/javascripts/product.js
@@ -1,21 +1,43 @@
 $(document).ready(function() {
 	
-	$('#productColor .selectionOptions').click(function(){
+	$("#productColor .selectionOptions").click(function(){
 		
-		$('#selectedProductColor').html($(this).text());
-		
-	});
-	
-	$('#productSize .selectionOptions').click(function(){
-		
-		$('#selectedProductSize').html($(this).text());
+		$("#selectedProductColor").html($(this).text());
 		
 	});
 	
-	$('#productQuantity .selectionOptions').click(function(){
+	$("#productSize .selectionOptions").click(function(){
 		
-		$('#selectedProductQuantity').html($(this).text());
+		$("#selectedProductSize").html($(this).text());
+		
+		quantitySelectionState();
 		
 	});
+	
+	$("#productQuantity .selectionOptions").click(function(){
+		
+		$("#selectedProductQuantity").html($(this).text());
+		
+	});
+	
+	quantitySelectionState();
 	
 });
+
+function quantitySelectionState(){
+	
+	if( $("#selectedProductSize").text() === "") {
+		
+		$("#labelProductQuantity input").prop("disabled", true);
+		
+		$("#labelProductQuantity").addClass("disabled");
+	}
+	else {
+		
+		$("#labelProductQuantity input").prop("disabled", false);
+		
+		$("#labelProductQuantity").removeClass("disabled");
+		
+	}
+	
+}
