f79cfcf5188a41bf4d28acefc42f1bc95043e413
maael <mae.me.uk@gmail.com>
2014-12-21 13:47:27 +0000

    Add outfit name to outfit view

--- D2/Outfit View II/javascripts/outfit.js
+++ D2/Outfit View II/javascripts/outfit.js
@@ -1,8 +1,36 @@
 window.onload = function () {
     $('.outfit-btn').click(function(){
-        $('.outfit-area').toggleClass('active');
+        var added = false;
+        if(!$('.outfit-area').hasClass('active')){
+            $('.outfit-area').addClass('active');
+            added = true;
+        }
+        $('.outfits').slideToggle("fast",function(){
+            if(!added){
+                $('.outfit-area').removeClass('active');
+            }
+        });
     });
     $('.basket-btn').click(function(){
-        $('.basket-area').toggleClass('active');
+        var added = false;
+        if(!$('.basket-area').hasClass('active')){
+            $('.basket-area').addClass('active');
+            added = true;
+        }
+        $('.basket').slideToggle("fast",function(){
+            if(!added){
+                $('.basket-area').removeClass('active');
+            }
+        });
     });
-}
+    $('.outfit-name').focus(function () {
+        $(this).parent().removeClass('saved').addClass('saving');    
+    });    
+    $('.outfit-name').focusout(function () {
+        if($(this).val()!==""){
+            $(this).parent().removeClass('saving').addClass('saved');
+        } else {
+            $(this).parent().removeClass('saving').removeClass('saved');
+        }
+    });
+};
