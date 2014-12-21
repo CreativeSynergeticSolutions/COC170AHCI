daf3986e2dac9b09dad189b9939c45dba75de938
maael <mae.me.uk@gmail.com>
2014-12-21 18:11:55 +0000

    Add basic outfits/basket management object
    Outfit on the Outfit 2 page now loads from management object.
    Need to change -
    - outfits, at the moment stores 1 outfit in entire outfits object. Need
    to change it so it's an array of outfit objects, not an array of item
    objects as it is at the moment.

--- D2/Outfit View II/javascripts/outfit.js
+++ D2/Outfit View II/javascripts/outfit.js
@@ -1,4 +1,38 @@
+var wall = new wall();
+function loadOutfit () {
+    var outfits = wall.getOutfits();
+    for(var i = 0; i < outfits.length; i++){
+        var item = outfits[i],
+            itemEle = document.createElement('div'),
+            rmvEle = document.createElement('div'),
+            rmvIcon = document.createElement('i');
+        itemEle.className = "main-item item";
+        itemEle.style.backgroundImage = "url('"+item.imgSrc+"')";
+        itemEle.dataset.itemname = item.name;
+        rmvEle.className = "remove-item";
+        rmvIcon.className = "icon-cancel";
+        rmvEle.appendChild(rmvIcon);
+        itemEle.appendChild(rmvEle);
+        document.getElementById(item.type+'-area').appendChild(itemEle);
+    }
+};
+function loadBasket() {
+    var basket = wall.getBasket();
+    console.log(basket);
+    for(var i = 0; i < basket.length; i++){
+        var item = basket[i],
+            itemEle = document.createElement('div');
+        itemEle.className = 'basket-item';
+        itemEle.innerHTML = item.name;
+        document.getElementById('basket').appendChild(itemEle);
+    }
+}
+function initialLoad() {
+    loadOutfit();
+    loadBasket();
+}
 window.onload = function () {
+    initialLoad();
     $('.outfit-btn').click(function(){
         var added = false;
         if(!$('.outfit-area').hasClass('active')){
@@ -32,5 +66,10 @@ window.onload = function () {
         } else {
             $(this).parent().removeClass('saving').removeClass('saved');
         }
+    });
+    $('.remove-item').on('click', function () {
+        console.log($(this).parent().data('itemname'));
+        wall.removeFromOutfit({'itemName':$(this).parent().data('itemname')});
+        $(this).parent().remove();
     });
 };
