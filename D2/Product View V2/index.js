2ce0a17a9b09df91f8be2facb3ec4997e70d1a95
Johnbastian <j.emilianus-12@student.lboro.ac.uk>
2014-12-28 21:45:18 +0000

    ProductPage V2 file clean up

--- D2/Product View V2/index.js
+++ D2/Product View V2/index.js
@@ -1,19 +1,89 @@
-var imgs = ["DressFront.jpg", "DressBack.png", "DressPattern.jpg"];
+var imgs = ["DressFront.png", "DressBack.png", "DressPattern.png"];
 
 $(document).ready(function(){
 	
-	$(".imgBtns").click(function(){
+	setupImageDisplay();
+	setupProductDetails();
+	
+});
+
+function setupImageDisplay() {
+	
+	for (var iC = 0; iC < imgs.length; iC++){
+		$("#ulImages").append("<div class='imgBtns' data-Img="+ iC + " ></div>");
+	}
+	
+	$(".imgBtns").each(function(){
 		
 		var index= $(this).attr("data-Img");
+		$(this).css("background-image", "url(productImages/" + imgs[index] + ")");
 		
-		$("#productBox").css("background-image", "url(productImages/" + imgs[index] + ")")
+		$(this).click(function(){$("#displayImg").css("background-image", "url(productImages/" + imgs[index] + ")")});
 		
 	});
 	
-	$("#frontImg").click();
+	$("#displayImgLabel input").change(function(){
+		
+		console.log("the state is: " + this.checked);
+		
+		if(this.checked) {
+			
+			$("#displayImg").panzoom("enable");
+			$("#displayImgLabel").css("cursor", "default");
+			document.getElementById("displayImgCheck").disabled = true;
+						
+		}
+		
+	});
 	
-	$("li[data-colour=1]").css("background-color", "white", "important");
-	$("li[data-colour=2]").css("background-color", "black", "important");
-	$("li[data-colour=3]").css("background-color", "grey",  "important");
+	$("#closeDisplayImgBtn").click(function(){
+		
+		document.getElementById("displayImgCheck").disabled = false;
+		
+		$("#displayImgLabel").css("cursor", "pointer");
+		
+		$("#displayImg").panzoom("reset");
+		$("#displayImg").panzoom("disable");
+		
+	});
+		
+	$(".imgBtns").first().click();
 	
-});
+
+	$panzoom = $("#displayImg").panzoom();
+	$panzoom.parent().on('mousewheel.focal', function( e ) {
+		e.preventDefault();
+		var delta = e.delta || e.originalEvent.wheelDelta;
+		var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+		$panzoom.panzoom('zoom', zoomOut, {
+	  		increment: 0.1,
+	 		 animate: false,
+	  		focal: e
+		});
+	});
+	$("#displayImg").panzoom("disable");
+	
+}
+
+function setupProductDetails() {
+	
+	$("#productSectionLabel input").change(function(){
+				
+		if(this.checked){
+			
+			$("#productSectionLabel").css("cursor", "default");
+			document.getElementById("productDetailsCheck").disabled = true;
+						
+		}
+		
+	});
+	
+	$("#closeProductDetailsBtn").click(function(){
+		
+		document.getElementById("productDetailsCheck").disabled = false;
+		
+		$("#productSectionLabel").css("cursor", "pointer");
+		
+	});
+	
+}
