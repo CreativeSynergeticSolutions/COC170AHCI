8cc60361bdbbe6012c6fa1c8f8cd1855268ab45c
Clowny <c.b.lawrie-12@student.lboro.ac.uk>
2014-12-29 12:51:26 +0000

    basket start - currently load all items from JSON in for testing.

--- D2/Final/javascripts/basket.js
+++ D2/Final/javascripts/basket.js
@@ -1,7 +1,7 @@
 var wall = new wall();
-
+var basket;
 function loadBasketList() {
-    var basket = wall.getBasket();
+    basket = wall.getBasket();
     console.log(basket);
 	console.log(items);
     if(basket!=null&&basket.length>0){
@@ -14,7 +14,36 @@ function loadBasketList() {
 		console.log("empty basket")
     }
 }
+function displayItems(){
+	var html = "";
+	for(var i = 0; i < basket.length;i++){
+		html += "<div class='item'>";
+		html += "<div class='price'>"+basket[i].price+"</div>";
+		html += "<div class='image'>";
+		html += "<img src='"+basket[i].images[0].front+"'>";
+		html += "</div>";
+		html += "<p>"+basket[i].name+"</p>";
+		html += "</div>";
+	}
+	$('#items').html(html);
+}
+
 
 window.onload = function () {
 	loadBasketList();
+	var items;
+	$.getJSON("data/items.json")//just for testing//////////////////////////////////////////
+	.error(function () {
+		console.log("Error in JSON");
+	})
+	.complete(function () {
+		items = (typeof items === undefined ? {} : items);
+	})
+	.success(function (data) {
+		items = data.items;
+		console.log(items);
+		console.log("success");
+		basket = items;
+		displayItems();
+	});/////////////////////////////////////////////////////////////////////////////////////
 }
