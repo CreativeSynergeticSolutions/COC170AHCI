c510164446cd208e420c271da8ac653affeccc29
maael <mae.me.uk@gmail.com>
2014-12-31 14:28:31 +0000

    Work on outfit/basket interfaces & outfit page

--- D2/Final/javascripts/outfit.js
+++ D2/Final/javascripts/outfit.js
@@ -1,26 +1,62 @@
 var wall = new wall();
-function loadOutfit () {
-    var outfits = wall.getOutfits();
-    if(outfits!=null&&outfits.length>0){
+
+function makeEmptyEle(text, className){
+    var emptyEle = document.createElement('div');
+    emptyEle.className = className;
+    emptyEle.innerHTML = text;
+    return emptyEle;
+}
+function makeItemListItem (item) {
+    console.log("Item" + item);
+    var itemEle = document.createElement('div');
+    itemEle.className = "current-item btn-link";
+    itemEle.setAttribute("data-href","product.html?"+item.item.productCode);
+    itemEle.innerHTML = item.item.name;
+    return itemEle;
+}
+function loadItemList(outfit){
+    var outfitItemListEle = document.getElementById("current-items");
+    outfitItemListEle.innerHTML = "";
+    if(outfit!=null&&outfit.items.length>0){
+        for(var i=0; i<outfit.items.length; i++){
+            console.log(outfit.items[i]);
+            outfitItemListEle.appendChild(makeItemListItem(outfit.items[i]));
+        }
+    } else {
+        outfitItemListEle.appendChild(makeEmptyEle('No items.','current-item'));
+    }
+}
+function loadOutfit (outfit) {
+    if(outfit!=null){
+        $('.outfit-name').val(outfit.name);
+    }
+    if(outfit!=null&&outfit.items.length>0){
         $('.outfit-trash-area').show();
-        for(var i = 0; i < outfits.length; i++){
-            var item = outfits[i],
+        for(var i = 0; i < outfit.items.length; i++){
+            var item = outfit.items[i].item;
                 itemEle = document.createElement('div'),
                 addEle = document.createElement('div'),
-                addIcon = document.createElement('i');
-            itemEle.className = "main-item item";
-            itemEle.style.backgroundImage = "url('"+item.imgURL+"')";
+                buyEle = document.createElement('div'),
+                addIcon = document.createElement('i'),
+                buyIcon = document.createElement('i');
+            itemEle.className = "main-item item btn-link";
+            itemEle.setAttribute("data-href","product.html?"+item.productCode);
+            itemEle.style.backgroundImage = "url('"+item.images.front+"')";
             itemEle.dataset.itemname = item.name;
             addEle.className = "add-item";
             addIcon.className = "icon-plus";
+            buyEle.className = "buy-item";
+            buyIcon.className = "icon-basket";
             addEle.appendChild(addIcon);
+            buyEle.appendChild(buyIcon);
             itemEle.appendChild(addEle);
+            itemEle.appendChild(buyEle);
             $('#'+item.type+'-area').find('.add-area').hide();
             document.getElementById(item.type+'-area').appendChild(itemEle);
         }
         $('.item').draggable({
             cursor: '-webkit-grabbing',
-            zIndex: 100,
+            zIndex: 5,
             drag: function (event, ui) {
                  $(this).css('transform','rotate(0deg)');
             },
@@ -31,36 +67,77 @@ function loadOutfit () {
             }
         });
     }
+    loadItemList(outfit);
 }
-function makeBasketItem(itemName) {
+function makeBasketItem(item) {
     var itemEle = document.createElement('div');
-    itemEle.className = 'basket-item';
-    itemEle.innerHTML = itemName;
+    itemEle.className = 'basket-item btn-link';
+    itemEle.setAttribute("data-href","product.html?"+item.item.productCode);
+    itemEle.innerHTML = item.item.name + " (" + item.size + ") " + "x" + item.quantity;
     return itemEle;
 }
 function loadBasketList() {
-    var basket = wall.getBasket();
-    console.log(basket);
+    var basket = wall.getBasket(),
+        basketEle = document.getElementById('basket');
+    basketEle.innerHTML = "";
     if(basket!=null&&basket.length>0){
         for(var i = 0; i < basket.length; i++){
-            var item = basket[i];
-            document.getElementById('basket').appendChild(makeBasketItem(item.name));
+            basketEle.appendChild(makeBasketItem(basket[i]));
         }
     } else {
-        document.getElementById('basket').appendChild(makeBasketItem('Your Basket is Empty'));
+        //document.getElementById('basket').appendChild(makeBasketItem('Your Basket is Empty'));
     }
 }
+function makeOutfitListItem(outfit, selected) {
+    var selected = selected || false,
+        itemEle = document.createElement('div'),
+        nameEle = document.createElement('span'),
+        toolsEle = document.createElement('span'),
+        eyeIcon = document.createElement('i'),
+        basketIcon = document.createElement('i'),
+        itemClasses = "outfit-item " + ((selected) ? "outfit-selected" : "");
+    itemEle.className = itemClasses;
+    nameEle.className = "left outfit-name";
+    toolsEle.className = "right outfit-tools";
+    eyeIcon.className = "icon-eye";
+    basketIcon.className = "icon-basket";
+    nameEle.innerHTML = outfit.name;
+    itemEle.appendChild(nameEle);
+    toolsEle.appendChild(eyeIcon);
+    toolsEle.innerHTML += " | ";
+    toolsEle.appendChild(basketIcon);
+    itemEle.appendChild(toolsEle);
+    return itemEle;
+}
 function loadOutfitList() {
-    var outfit = wall.getOutfits();
-    console.log(outfit);
+    var outfits = wall.getOutfits(),
+        outfitsEle = document.getElementById('outfits');
+    outfitsEle.innerHTML = "";
+    if(outfits!=null&&outfits.length>0){
+        for(var i=0; i<outfits.length; i++){
+            var selected = (i==0 ? true : false);
+            outfitsEle.appendChild(makeOutfitListItem(outfits[i], selected));
+        }
+    } else {
+        //document.getElementById('outfits').appendChild(makeOutfitListItem('You have no outfits'));
+    }
 }
 function initialLoad() {
-    loadOutfit();
     loadBasketList();
     loadOutfitList();
+    var outfitName = $.trim($('.outfit-selected>.outfit-name').text());
+    wall.setCurrentOutfit(outfitName);
+    loadOutfit(wall.getCurrentOutfit());
 }
 window.onload = function () {
+    $('.main-area').hide().fadeIn(500);
     initialLoad();
+    $('.btn-link').click(function (e) {
+        newLocation = $(this).data('href');
+        $('.main-area').fadeOut(500, function () {
+            window.location = newLocation;
+        });
+    });
     $('.outfit-btn').click(function(){
         var added = false;
         if(!$('.outfit-area').hasClass('active')){
@@ -110,6 +187,18 @@ window.onload = function () {
     $('.add-item').on('click', function () {
 
     });
+    $('.buy-item').on('click', function () {
+        var itemName = $(this).parent().data('itemname');
+        var outfit = wall.getCurrentOutfit();
+        for(var i=0; i<outfit.items.length; i++){
+            var item = outfit.items[i];
+            if(item.item.name==itemName){
+                wall.addToBasket(item.item, item.quantity, item.size, item.colour);
+                loadBasketList();
+                break;
+            }
+        }
+    });
     $('.outfit-trash-area').droppable({
         accept: '.item',
         tolerance: 'touch',
@@ -118,6 +207,7 @@ window.onload = function () {
             $(ui.draggable[0]).find('.add-item').fadeOut("fast", function () {});
         },
         drop: function (event, ui) {
+            $('.outfit-trash-area i').removeClass('icon-trash').addClass('icon-trash-empty');
             //wall.removeFromOutfit({'itemName':$(this).parent().data('itemname')});
             ui.draggable[0].remove();
         },
--- D2/Final/javascripts/wall.js
+++ D2/Final/javascripts/wall.js
@@ -1,10 +1,27 @@
+/*
+** Java Hashcode implementation in JavaScript
+*/
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr, len;
+  if (this.length == 0) return hash;
+  for (i = 0, len = this.length; i < len; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
+/*
+**  Generic Interfaces
+*/
 var wall = function (options) {
     options = options || {};
     this.user = options.user || '';
     this.userStorage = 'AHCI'+'-'+((this.user=='')?'local':this.user);
     this.saveObjectToStorage = function (name, toSave) {
-        this.wallLog('Saving '+name+'.');
-        this.wallLog('Getting existing '+name+'.');
+        this.wallLog('Saving '+name);
+        this.wallLog('Getting existing '+name);
         var existing = this.getObjectFromStorage();
         if(existing===null){
             existing = {'basket':[],'outfits':[]};
@@ -30,28 +47,28 @@ var wall = function (options) {
         console.log('%c Wall | %c'+message+' ',wallStyle,messageStyle);
     };
 };
-wall.prototype.addToOutfit = function (outfit) {
-    this.wallLog('Adding an outfit.');
-    var outfits = this.getOutfits();
-    if(outfits===null){
-        outfits = [];
+
+/*
+**  Outfits Interfaces
+*/
+wall.prototype.addOutfitItem = function (outfitName, item, quantity, size, colour) {
+    this.wallLog('Adding an outfit');
+    var entry = {
+            "item": item,
+            "quantity": quantity,
+            "size": size,
+            "colour": colour
+        },
+        outfits = this.getOutfits(),
+        outfitIndex = this.getOutfitIndex(outfitName);
+        outfit = outfits[outfitIndex];
+    if(outfit===null){
+        this.addOutfit(outfitName);
     }
-    outfit.id = outfits.length;
-    outfits.push(outfit);
+    outfit.items.push(entry);
     this.saveObjectToStorage('outfits',outfits);
 };
-wall.prototype.removeOutfit = function (outfitName) {
-    this.wallLog('Removing an outfit.');
-    var outfits = this.getOutfits();
-    for(var i = 0; i < outfits.length; i++) {
-        if(outfits[i].name == outfitName){
-            outfits.splice(i, 1);
-            break;
-        }
-    }
-    this.saveObjectToStorage('outfits',outfits);
-};
-wall.prototype.removeFromOutfit = function (item) {
+wall.prototype.removeOutfitItem = function (item) {
     this.wallLog('Removing '+item.name+' from an outfit.');
     var outfits = this.getOutfits();
     for(var i = 0; i < outfits.length; i++) {
@@ -62,13 +79,65 @@ wall.prototype.removeFromOutfit = function (item) {
     }
     this.saveObjectToStorage('outfits',outfits);
 };
+wall.prototype.getOutfitItems = function (outfitName) {
+    this.wallLog('Getting outfit items | '+outfitName);
+    var outfit = this.getOutfit(outfitName);
+    return outfit.items;
+};
+wall.prototype.addOutfit = function (outfitName) {
+    this.wallLog('Adding outfit | '+outfitName);
+    var outfits = this.getOutfits();
+    if(outfits===null){
+        outfits = [];
+    }
+    outfits.push({"name":outfitName, items:[]});
+    this.saveObjectToStorage('outfits',outfits);
+};
+wall.prototype.removeOutfit = function (outfitName) {
+    this.wallLog('Removing outfit | '+outfitName);
+    var outfits = this.getOutfits();
+    for(var i = 0; i < outfits.length; i++) {
+        if(outfits[i].name == outfitName){
+            outfits.splice(i, 1);
+            break;
+        }
+    }
+    this.saveObjectToStorage('outfits',outfits);
+};
+wall.prototype.getOutfitIndex = function (outfitName) {
+    this.wallLog('Getting outfit index | '+outfitName);
+    var outfits = this.getOutfits(),
+        index = null;
+    for(var i=0; i<outfits.length;i++){
+        if(outfits[i].name==outfitName){
+            index = i;
+            break;
+        }
+    }
+    return index;
+};
+wall.prototype.getOutfit = function (outfitName) {
+    this.wallLog('Getting outfit | '+outfitName);
+    var outfits = this.getOutfits(),
+        outfitIndex = this.getOutfitIndex(outfitName);
+    return outfits[outfitIndex];
+};
 wall.prototype.getOutfits = function () {
-    this.wallLog('Getting outfits.');
+    this.wallLog('Getting outfits');
     var userStorage = this.getObjectFromStorage('outfits');
     return (userStorage===null ? null : userStorage);
 };
+wall.prototype.getCurrentOutfit = function () {
+    return this.getObjectFromStorage('currentOutfit');
+}
+wall.prototype.setCurrentOutfit = function (outfitName) {
+    this.saveObjectToStorage('currentOutfit', this.getOutfit(outfitName));
+}
+/*
+**  Basket Interfaces
+*/
 wall.prototype.addToBasket = function (item, quantity, size, colour) {
-    this.wallLog('Adding item to basket.');
+    this.wallLog('Adding item to basket');
     var entry = {
         "item": item,
         "quantity": quantity,
@@ -88,7 +157,7 @@ wall.prototype.addToBasket = function (item, quantity, size, colour) {
     }
 };
 wall.prototype.removeItemFromBasket = function (entry) {
-    this.wallLog('Removing item from basket.');
+    this.wallLog('Removing item from basket');
     var basket = this.getBasket();
     for(var i = 0; i < basket.length; i++) {
         var check = (basket[i].item.name == entry.item.name)
@@ -104,13 +173,13 @@ wall.prototype.removeItemFromBasket = function (entry) {
     this.saveObjectToStorage('basket',basket);
 }
 wall.prototype.updateBasket = function (items) {
-    this.wallLog('Updating basket.');
+    this.wallLog('Updating basket');
     for(var i = 0; i < items.length; i++){
         this.addToBasket(items[i]);
     }
 };
 wall.prototype.getBasket = function () {
-    this.wallLog('Getting basket.');
+    this.wallLog('Getting basket');
     var userStorage = this.getObjectFromStorage('basket');
     return (userStorage===null ? null : userStorage);
 };
