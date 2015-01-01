e823d5184933d7d610fa3d60dacf001a067fb1e2
Max Pearson <m.pearson-11@student.lboro.ac.uk>
2015-01-01 14:37:53 +0000

    Complete Overahall on ShelfView
    Linking up items.json to search and show in ShelfView

--- D2/ShelfView/categories.js
+++ D2/ShelfView/categories.js
@@ -5,18 +5,8 @@ var currentSelection="";
 
 var CAT={
 
-	"Christmas": [
-		"Women",
-		"Men",
-		"Boys",
-		"Girls",
-		"Baby",
-		"Christmas Jumpers",
-		"Novelty",
-		"Gifts by Price",
-		"Home Decor",
-		"Trees and Decorations"
-	],
+	
+	
 	"Home and Garden":[
 		"Just Arrived",
 		"Furnishings",
@@ -45,7 +35,7 @@ var CAT={
 		"Help to Buy",
 		"Underwear and Lingerie",
 		"Nightwear and Slippers",
-		"Christmas",
+		
 		"Offers"
 	],
 	"Men":[
@@ -55,7 +45,7 @@ var CAT={
 		"Brands",
 		"Collections",
 		"Offers",
-		"Christmas",
+		
 		"Personal Grooming"
 	],
 	"Boys":[
@@ -86,7 +76,7 @@ var CAT={
 		"Girls",
 		"Boys",
 		"Collections",
-		"Christmas",
+
 		"Offers",
 		"Nursery"
 	],
@@ -100,7 +90,7 @@ var CAT={
 	"Fancy Dress":[
 		"Fancy Dress",
 		"Fancy Dress Accessories",
-		"Christmas"
+
 	],
 	"Sale":[
 		"Shop by"
@@ -110,141 +100,437 @@ var CAT={
 };
 var SUB_CAT={
 
-	//category_subcategory
-	"Christmas_Women":
-		[
-			"Shop all Gifts",
-			"Accessories",
-			"Coats and Jackets",
-			"Dresses and Jumpsuits",
-			"Jumpers and Cardigans",
-			"Lingerie",
-			"Nightwear and Slippers",
-			"Onesies"
+	
 
-		],
-	"Christmas_Men":[
-		"Shop all Gifts",
+	"Home and Garden_Just Arrived":[
+		"New In"
+	],
+	"Home and Garden_Furnishings":[
+		"Candles and Diffusers",
+		"Curtains", 
+		"Cushions and Throws",
+		"Frames and Albums",
+		"Home and Accessories",
+		"Lighting",
+		"Mirrors",
+		"Paint and Wallpaper",
+		"Rugs, Runners & Mats",
+		"Storage",
+		"Vases and Ornaments"
+	],
+	"Home and Garden_Office":[
+		"View all Office"
+	],
+	"Home and Garden_Bedroom":[
+		"Bedding",
+		"Beds", 
+		"Bedside Tables",
+		"Chest of Drawers",
+		"Dressing Tables",
+		"Duvets & Pillows",
+		"Sofabeds",
+		"Wardrobes"
+	],
+	"Home and Garden_Bathroom":[
+		"Bathroom Accessories",
+		"Bathroom Fittings",
+		"Shower Curtains",
+		"Showers",
+		"Toilet Seats",
+		"Towels and Bath Mats"
+	],
+	"Home and Garden_Kids":[
+		"View all Kids",
+		"Bedroom",
+		"Dining",
+		"Furniture"
+	],
+	"Home and Garden_Nursery":[
+		"Bedding & Blankets",
+		"Changing Units",
+		"Cots and Matresses",
+		"Nursing Chairs",
+		"Nursery Decor",
+		"Towels",
+		"Wardrobes"
+		
+	],
+	"Home and Garden_Outdoor":[
+		"View all Outdoor"
+	],
+	"Home and Garden_Living Room":[
+		"Bookcases & Cabinets",
+		"Coffee & Side Tables",
+		"Footstools & Pouffes",
+		"Sideboards & Storage",
+		"Sofas & Armchairs",
+		"TV Units"
+	],
+	"Home and Garden_Appliances":[
+		"Blenders & Processors",
+		"Coffee Machines",
+		"Cooking Appliances",
+		"Fans & Heating",
+		"Irons",
+		"Kettles",
+		"Microwaves",
+		"Toasters",
+		"Vacuums & Steam Mops"
+	],
+	"Home and Garden_Kitchen and Dining":[
+		"Baking",
+		"Bar Stools",
+		"Bins",
+		"Cleaning",
+		"Cups & Mugs",
+		"Cutlery",
+		"Dining Tables & Chairs",
+		"Glassware",
+		"Laundry",
+		"Ovenware",
+		"Tableware",
+		"Utensils & Gadgets"
+	],
+
+	"Women_Clothing":[
+		"New In",
 		"Accessories",
-		"Coats and Jackets",
-		"Jumpers and Cardigans",
-		"Nightwear and Slippers",
+		"Blouses & Shirts",
+		"Coats & Jackets",
+		"Dresses",
+		"Hats, Gloves & Scarves",
+		"Jeans",
+		"Jumpers & Cardigans",
+		"Jumpsuits & Playsuits",
+		"Leggings",
+		"Maternity",
+		"Nightwear & Slippers",
 		"Onesies",
+		"Plus Size",
+		"Skirts",
+		"Swimwear",
 		"Tops",
-		"Underwear and Socks"
+		"Trousers & Shorts",
+		"Tunics",
+		"Last Chance To Buy"
 	],
-	"Christmas_Boys":[
-		"Shop all Gifts",
-		"Accessories",
-		"Coats and Jackets",
+	"Women_Footwear":[
+		"All Footwear",
+		"Boots & Wellies",
+		"Flats",
+		"Heels",
+		"Sandals & Flip-flops",
+		"Shoes",
+		"Slippers",
+		"Trainers"
+	],
+	"Women_Collections":[
+		"Character Shop",
 		"Fancy Dress",
-		"Jumpers and Cardigans",
-		"Nightwear and Slippers",
+		"Graduate Fashion Week",
+		"Holiday Shop",
+		"Sportswear",
+		"The Contemporary Collection",
+		"Thermal",
+		"Tickled Pink",
+		"Winter Warmers",
+		"Workwear"
+	],
+	
+	"Women_Latest Trends":[
+		"Autumn"
+	],
+	"Women_Shop By Fit":[
+		"Maternity",
+		"Petite",
+		"Plus Size"
+	],
+	"Women_Offers":[
+		"Mix & Match Lingerie - 3 for £6",
+		"Mix & Match Nightwear - 2 for £10",
+		"Mix & Match Socks - 4 for £4.50"
+	],
+	"Women_Electrical Beauty":[
+		"Hairdryers",
+		"Hair Removal",
+		"Hair Straighteners",
+		"Hair Styling",
+		"Spa and Massage"
+	],
+
+	"Lingerie_Bras":[
+		"All Bras",
+		"Balcony Bras",
+		"Branded Bras",
+		"DD+ Bras",
+		"Nursing Bras",
+		"Plunge Bras",
+		"Post Surgery",
+		"Sports Bras",
+		"T-Shirt Bras"
+	],
+	"Lingerie_Help to Buy":[
+		"Fit Guide"
+	],
+	"Lingerie_Underwear and Lingerie":[
+		"Branded Lingerie",
+		"Knickers",
+		"Lingerie Sets",
+		"Lingerie Solutions",
+		"Shapewear",
+		"Socks",
+		"Tights",
+		"Thermals"
+		
+	],
+	"Lingerie_Nightwear and Slippers":[
+		"All Nightwear",
+		"Dressing Gowns",
+		"Nightdresses",
 		"Onesies",
+		"Pyjamas",
+		"Slippers"
+	],
+
+	"Lingerie_Offers":[
+		"Mix and Match Lingerie - 3 for £6",
+		"Mix and Match Nightwear - 2 for £10",
+		
+	],
+
+	"Men_Clothing":[
+		"New In",
+		"Accessories",
+		"Coats & Jackets",
+		"Jeans",
+		"Jumpers & Cardigans",
+		"Nightwear & Slippers",
+		"Onesies",
+		"Shirts",
+		"Socks",
+		"Suits & Tailoring",
+		"Sweatshirts & Hoodies",
+		"Swimwear",
+		"Ties",
+		"Trousers & Shorts",
+		"T-Shirts & Polos",
+		"Underwear & Polos",
+		"Underwear",
+		"Last Chance To Buy"
+	],
+	"Men_Footwear":[
+		"All Footwear",
+		"Boots",
+		"Casual Shoes",
+		"Formal Shoes",
+		"Slippers",
+		"Trainers"
+	],
+	"Men_Brands":[
+		"Boston Crew"
+	],
+	"Men_Collections":[
+		"Character Shop",
+		"Fancy Dress",
+		"Officewear",
+		"Outdoor Workwear",
+		"Sportswear",
+		"Thermal",
+		"Winter Warmers"
+	],
+	"Men_Offers":[
+		
+	],
+
+	"Men_Personal Grooming":[
+		"Clippers, Trimmers and Shavers"
+	],
+
+	"Boys_Clothing":[
+		"New In",
+		"Accessories",
+		"Coats & Jackets",
+		"Jeans",
+		"Jumpers & Cardigans",
+		"Nightwear & Slippers",
+		"Onesies",
+		"Outfits",
+		"Shirts",
+		"Suits",
+		"Sweatshirts & Hoodies",
+		"Swimwear",
+		"Trousers & Shorts",
+		"Underwear & Socks",
+	],
+	"Boys_Footwear":[
+		"All Footwear",
+		"Boots & Wellies",
+		"Sandals & Flip-flops",
+		"School Shoes",
+		"Trainers & Pumps"
+	],
+	"Boys_School":[
+		"Blazers & Coats",
+		"Jumpers",
+		"Polo Shirts",
+		"Shirts",
+		"Shoes & Trainers",
+		"Shorts",
+		"Sportswear",
+		"Sweatshirts",
+		"Trousers",
+		"Underwear & Socks"
+	],
+	"Boys_Collections":[
+		"Winter Warmers"
+	],
+
+	"Boys_Shop by":[
+		"Last Chance to Buy"
+	],
+	"Boys_Character":[
+		"Character Shop"
+	],
+	"Boys_Kids Home":[
+		"Furniture",
+		"Dining",
+		"Bedroom",
+		"Wooden Toys"
+	],
+
+	"Girls_Clothing":[
+		"New In",
+		"Accessories",
+		"Coats & Jackets",
+		"Dancewear",
+		"Dresses & Outfits",
+		"Fancy Dress",
+		"Jeans",
+		"Jumpers & Cardigans",
+		"Leggings",
+		"Nightwear & Slippers",
+		"Onesies",
+		"Playsuits & Jumpsuits",
+		"Skirts",
+		"Sweatshirts & Hoodies",
+		"Swimwear",
 		"Tops",
+		"Trousers & Shorts",
+		"Underwear & Socks"
+
 	],
-	"Christmas_Girls":[
-		"Shop all Gifts",
+	"Girls_Footwear":[
+		"All Footwear",
+		"Boots & Wellies",
+		"Sandals & Flip-flops",
+		"School Shoes",
+		"Shoes",
+		"Trainers & Pumps",
+		
+	],
+	"Girls_School":[
 		"Accessories",
-		"Coats and Jackets",
-		"Fancy Dress",
+		"Blazers & Coats",
+		"Dancewear",
+		"Gingham Dresses",
 		"Jumpers and Cardigans",
-		"Nightwear and Slippers",
-		"Onesies",
+		"Pinafores",
+		"Polo Shirts",
+		"Shirts and Blouses",
+		"Shoes & Trainers",
+		"Shorts",
+		"Skirts",
+		"Sportswear",
+		"Sweatshirts",
+		"Trousers",
+		"Underwear & Socks"
 	],
-	"Christmas_Baby":[
-		"Shop all Gifts",
+	"Girls_Collections":[
+		"Frozen",
+		"One Direction",
+		"Winter Warmers"
+	],
+
+	"Girls_Shop by":[
+		"Last Chance to Buy"
+	],
+	"Girls_Character":[
+		"Character Shop"
+	],
+	"Girls_Kids Home":[
+		"Furniture",
+		"Dining",
+		"Bedroom",
+		"Wooden Toys"
+	],
+
+	"Baby_New In":[
+		"New In",
+		"Last Chance to Buy"
+	],
+	"Baby_New Baby":[
+		"All in Ones & Outfits",
+		"Bodysuits",
+		"Hospital List",
+		"Sleepsuits",
+		"Starter Packs",
+	],
+	"Baby_Baby Accessories":[
+		"Shoes",
+		"Bibs & Muslim Squares",
+		"Blankets & Shawls",
+		"Sleeping Bags",
+		"Socks & Tights"
+	],
+	"Baby_Boys":[
 		"Accessories",
 		"All in Ones",
-		"Jumpers and Cardigans",
-		"Coats and Pramsuits",
-		"Fancy Dress",
+		"Bodysuits",
+		"Coats & Pramsuits",
+		"Cardigans & Jumpers",
 		"Outfits",
-		"Sleepsuits and Pyjamas"
+		"Sleepsuits & Pyjamas",
+		"Swimwear",
+		"Tops",
+		"Trousers & Jeans"
 	],
-	"Christmas_Christmas Jumpers":[],
-	"Christmas_Novelty":[],
-	"Christmas_Gifts by Price":[],
-	"Christmas_Home Decor":[],
-	"Christmas_Trees and Decorations":[],
+	"Baby_Girls":[
+		"Accessories",
+		"All in Ones",
+		"Bodysuits",
+		"Coats & Pramsuits",
+		"Dresses and Outfits",
+		"Cardigans & Jumpers",
+		"Leggings & Jeans",
+		"Sleepsuits & Pyjamas",
+		"Socks and Tights",
+		"Swimwear",
+		"Tops",
+	],
+	
+	"Baby_Collections":[
+		"Character Shop",
+		"Dreamskin",
+		"Fancy Dress",
+		"Premature",
+		"Winter Warmers"
+	],
 
-	"Home and Garden_Just Arrived":[],
-	"Home and Garden_Furnishings":[],
-	"Home and Garden_Office":[],
-	"Home and Garden_Bedroom":[],
-	"Home and Garden_Bathroom":[],
-	"Home and Garden_Kids":[],
-	"Home and Garden_Nursery":[],
-	"Home and Garden_Outdoor":[],
-	"Home and Garden_Living Room":[],
-	"Home and Garden_Appliances":[],
-	"Home and Garden_Kitchen and Dining":[],
+	"Baby_Nursery":[
+		"Bedding & Blankets",
+		"Changing Units",
+		"Cots & Matresses",
+		"Nursery Decor",
+		"Nursing Chairs",
+		"Towels",
+		"Wardrobes"
+	],
 
-	"Women_Clothing":[],
-	"Women_Footwear":[],
-	"Women_Christmas":[],
-	"Women_Collections":[],
-	"Women_Latest Trends":[],
-	"Women_Shop By Fit":[],
-	"Women_Offers":[],
-	"Women_Electrical Beauty":[],
-
-	"Lingerie_Bras":[],
-	"Lingerie_Help to Buy":[],
-	"Lingerie_Underwear and Lingerie":[],
-	"Lingerie_Nightwear and Slippers":[],
-	"Lingerie_Christmas":[],
-	"Lingerie_Offers":[],
-
-	"Men_Clothing":[],
-	"Men_Footwear":[],
-	"Men_Christmas":[],
-	"Men_Brands":[],
-	"Men_Collections":[],
-	"Men_Offers":[],
-	"Men_Christmas":[],
-	"Men_Personal Grooming":[],
-
-	"Boys_Clothing":[],
-	"Boys_Footwear":[],
-	"Boys_School":[],
-	"Boys_Collections":[],
-	"Boys_Christmas":[],
-	"Boys_Shop by":[],
-	"Boys_Character":[],
-	"Boys_Kids Home":[],
-
-	"Girls_Clothing":[],
-	"Girls_Footwear":[],
-	"Girls_School":[],
-	"Girls_Collections":[],
-	"Girls_Christmas":[],
-	"Girls_Shop by":[],
-	"Girls_Character":[],
-	"Girls_Kids Home":[],
-
-	"Baby_New in":[],
-	"Baby_New Baby":[],
-	"Baby_Baby Accessories":[],
-	"Baby_Girls":[],
-	"Baby_Boys":[],
-	"Baby_Collections":[],
-	"Baby_Christmas":[],
-	"Baby_Offers":[],
-	"Baby_Nursery":[],
-
-	"School_Boys":[],
-	"School_Girls":[],
-	"School_Older Boys":[],
-	"School_Older Girls":[],
-	"School_Last Chance":[],
-	"School_Fancy Dress":[],
-	"School_Fancy Dress":[],
-	"School_Fancy Dress Accessories":[],
-	"School_Christmas":[],
-
-	"Sale_Shop by":[]
-
+	"School_Accessories":[
+		"Lunch Boxes & Bottles",
+		"School Bags",
+		"Stationery & Craft"
+	]
 
 };
 
