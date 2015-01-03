919fc487d874af5df06079bcecccfdb47400f399
Clowny <c.b.lawrie-12@student.lboro.ac.uk>
2015-01-03 17:20:25 +0000

    fixed error.

--- D2/Final/javascripts/basket.js
+++ D2/Final/javascripts/basket.js
@@ -3,7 +3,6 @@ var basket;
 function loadBasketList() {
     basket = wall.getBasket();
     console.log(basket);
-	console.log(items);
     if(basket!=null&&basket.length>0){
         /* for(var i = 0; i < basket.length; i++){
             var item = basket[i];
@@ -17,13 +16,13 @@ function loadBasketList() {
 function loadModal(index){
 	var item = basket[index];
 	$("#container").css('display','block');
-	$("#inputText").html(item.items.name);
-	$("#paragraph").html(item.items.description[0].content);
-	$("#productCode").html(item.items.productCode);
-	$("#image").html("<img src='"+item.items.images.front+"'>");
+	$("#inputText").html(item.item.name);
+	$("#paragraph").html(item.item.description[0].content);
+	$("#productCode").html(item.item.productCode);
+	$("#image").html("<img src='"+item.item.images.front+"'>");
 	var bullets = "<ul>";
-	for (var i = 1; i < item.items.description.length; i++){
-		bullets += "<li>"+item.items.description[i].content+"</li>";
+	for (var i = 1; i < item.item.description.length; i++){
+		bullets += "<li>"+item.item.description[i].content+"</li>";
 	}
 	bullets += "</ul>"
 	$("#bullet").html(bullets);
@@ -34,7 +33,7 @@ function deleteItem(index){
 	$("#item"+index).remove();
 	var current = ($("#total").text()).substring(9,($("#total").text()).length);
 	console.log(current);
-	var total = parseInt(current) - (parseInt(basket[index].items.price.substring(1,basket[index].items.price.length)))*basket[index].quantity;
+	var total = parseInt(current) - (parseInt(basket[index].item.price.substring(1,basket[index].item.price.length)))*basket[index].quantity;
 
 	$("#total").html("Total: &#163 "+ add00(total));
 }
@@ -53,18 +52,18 @@ function displayItems(){
 		html += "<div class='item' id='item"+i+"'>";
 		html += "<div class='delete' onclick='deleteItem("+i+")'><div><span title='Delete Item'><i class='icon-trash-empty'></i></span></div></div>";
 		html += "<div class='info' onclick='loadModal("+i+")'><div><span title='Product info'><i class='icon-info'></i></span></div></div>";
-		html += "<div class='price'>"+basket[i].items.price+"</div>";
+		html += "<div class='price'>"+basket[i].item.price+"</div>";
 		html += "<div class='image'>";
-		html += "<img src='"+basket[i].items.images.front+"'>";
+		html += "<img src='"+basket[i].item.images.front+"'>";
 		html += "</div>";
-		html += "<p>"+basket[i].items.name+"</p>";
+		html += "<p>"+basket[i].item.name+"</p>";
 		html += "<a class='dial'>"+basket[i].quantity+"</a>";
 		html += "<p class='product'>Size: "+basket[i].size+"</p>";
 		html += "<p class='product'>Colour: "+basket[i].colour.name+"</p>";
-		html += "<p class='product'>Product Code: "+basket[i].items.productCode+"</p>";
+		html += "<p class='product'>Product Code: "+basket[i].item.productCode+"</p>";
 		html += "</div>";
 		//need quantity
-		total += parseInt((basket[i].items.price).substring(1,basket[i].items.price.length))*basket[i].quantity;
+		total += parseInt((basket[i].item.price).substring(1,basket[i].item.price.length))*basket[i].quantity;
 	}
 	$('#items').html(html);
 	$('#total').html("Total: &#163 "+add00(total))
